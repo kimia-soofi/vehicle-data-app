7d0f0949ce1be3e858a56f257d78e0b872cb3a2c
kimia-soofi <152678105+kimia-soofi@users.noreply.github.com>
2025-08-14 21:54:23 +0330

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+# نام کاربری و رمز عبور ادمین (می‌توانی اینجا عوض کنی
+# یا روی هاست به‌صورت ENV ست کنی: ADMIN_USERNAME / ADMIN_PASSWORD)
+ADMIN_USERNAME = "admin"
+ADMIN_PASSWORD = "1234"
+
+# فهرست مدل‌های خودرو که در منوی کشویی نمایش داده می‌شود
+CAR_MODELS = ["J4", "X5", "J6", "EAGLE"]
