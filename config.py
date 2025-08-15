b9686bea28b8edaa9fb8e9aada4fe59856be6ed1
kimia-soofi <152678105+kimia-soofi@users.noreply.github.com>
2025-08-15 21:03:55 +0330

    config.py

--- config.py
+++ config.py
@@ -1,9 +1,16 @@
 # نام کاربری و رمز عبور ادمین (می‌توانی اینجا عوض کنی یا ENV ست کنی)
-ADMIN_USERNAME = "admin"
-ADMIN_PASSWORD = "1234"
+import os
+
+# نام کاربری و رمز عبور ادمین و همکاران از ENV گرفته می‌شود
+ADMIN_USERNAME = os.environ.get("ADMIN_USERNAME", "admin_default")
+ADMIN_PASSWORD = os.environ.get("ADMIN_PASSWORD", "admin_pass_default")
+
+STAFF_USERNAME = os.environ.get("STAFF_USERNAME", "staff_default")
+STAFF_PASSWORD = os.environ.get("STAFF_PASSWORD", "staff_pass_default")
 
 # فایل مدل‌های خودرو
 CAR_MODELS_FILE = "car_models.json"
 
 # مدل‌های اولیه در صورت نبود فایل
-INITIAL_CAR_MODELS = ["J4", "X5", "J6", "EAGLE"]
+INITIAL_CAR_MODELS = ["J4", "X5", "T9", "EAGLE"]
+
