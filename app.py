c4a9d405722251bfc91db51f5c5fd9d8ceadba3c
kimia-soofi <152678105+kimia-soofi@users.noreply.github.com>
2025-08-29 21:04:34 +0330

    app.py

--- app.py
+++ app.py
@@ -181,7 +181,7 @@ def download_pdf(model, fname):
     font_path = os.path.abspath(os.path.join("static", "Vazirmatn-Regular.ttf"))
     
     left_margin = 0
-    right_margin = 0
+    right_margin = 25
     table_width = 595     # عرض A4 ~595pt - left_margin - right_margin
     col_ratios = [0.06, 0.32, 0.32, 0.1, 0.2]     # نسبت ستون‌ها به عرض جدول
 
@@ -352,6 +352,7 @@ def admin_logout():
 
 if __name__=="__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
+
 
 
 
